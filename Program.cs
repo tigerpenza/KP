56c4ebad1e9533036e98ed598f4e257271f4d5a7
evgenii <tigerpenza@gmail.com>
2022-10-04 09:51:07 +0300

    добавили два метода создание массива и вывод

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using static System.Console;
+
+string[] ChangeArray(string[] array)   
+{                                                  
+    int sizeNewArray = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+            sizeNewArray++;
+
+    string[] newArray = new string[sizeNewArray];
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    return newArray;
+}
+
+void ShowArray(string[] array)
+{
+    Write($"[{String.Join(", ", array)}]");
+}
+
